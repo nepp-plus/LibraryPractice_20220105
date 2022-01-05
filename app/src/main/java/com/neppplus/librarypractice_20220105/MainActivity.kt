ed80dc7b258dd82a276097a12b547175da242401
nepp-plus <nepp.plus@gmail.com>
2022-01-05 15:47:47 +0900

    권한 여부에 따른 행동 방침 준비.

--- app/src/main/java/com/neppplus/librarypractice_20220105/MainActivity.kt
+++ app/src/main/java/com/neppplus/librarypractice_20220105/MainActivity.kt
@@ -4,7 +4,9 @@ import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.bumptech.glide.Glide
+import com.gun0912.tedpermission.PermissionListener
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -21,9 +23,31 @@ class MainActivity : AppCompatActivity() {
 
 //            CALL (바로 전화 연결) 에 대한 권한 획득 후에 전화 연결 진행.
 
-            val myUri = Uri.parse("tel:010-2222-3333")
-            val myIntent = Intent( Intent.ACTION_CALL, myUri )
-            startActivity(myIntent)
+
+//            pl변수에, 권한 OK일때/ 거부일때 어떤 행동을 할지, 방침만 저장해둠.
+//            실제로 권한을 묻는 행위는 코딩 X
+
+            val pl = object : PermissionListener {
+                override fun onPermissionGranted() {
+
+//                    권한이 OK 되었을때 해줄일 적는 공간.
+
+                    val myUri = Uri.parse("tel:010-2222-3333")
+                    val myIntent = Intent( Intent.ACTION_CALL, myUri )
+                    startActivity(myIntent)
+
+                }
+
+                override fun onPermissionDenied(deniedPermissions: MutableList<String>?) {
+
+                    Toast.makeText(this@MainActivity, "권한이 거부되어 통화가 불가합니다.", Toast.LENGTH_SHORT)
+                        .show()
+
+                }
+
+            }
+
+
 
         }
 
