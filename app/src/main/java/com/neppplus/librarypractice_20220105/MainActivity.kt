f9196947be955eaa387bf0b8df2f0c19c1613663
nepp-plus <nepp.plus@gmail.com>
2022-01-05 10:56:37 +0900

    Glide 라이브러리 설치 / 코틀린코드 작성  - 이미지가 아직 안불러와짐.

--- app/src/main/java/com/neppplus/librarypractice_20220105/MainActivity.kt
+++ app/src/main/java/com/neppplus/librarypractice_20220105/MainActivity.kt
@@ -3,6 +3,7 @@ package com.neppplus.librarypractice_20220105
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -18,6 +19,13 @@ class MainActivity : AppCompatActivity() {
             startActivity(myIntent)
 
         }
+
+
+//        Glide 이용 => 웹의 이미지를 -> imgRecent에 반영하기.
+
+        Glide.with(this).load("https://cdn.kado.net/news/photo/201402/669003_243541_1228.jpg").into(imgRecent)
+
+
 
     }
 }
