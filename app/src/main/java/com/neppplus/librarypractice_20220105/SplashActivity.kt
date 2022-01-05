f33829270cf3b4b36727bbfb14be200c3519541a
nepp-plus <nepp.plus@gmail.com>
2022-01-05 14:53:14 +0900

    Splash > Main 이동   - 너무 빨리 이루어져서, 바로 메인으로 가는것처럼 보임 (문제발생)

--- app/src/main/java/com/neppplus/librarypractice_20220105/SplashActivity.kt
+++ app/src/main/java/com/neppplus/librarypractice_20220105/SplashActivity.kt
@@ -1,5 +1,6 @@
 package com.neppplus.librarypractice_20220105
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +8,12 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        val myIntent = Intent(this, MainActivity::class.java)
+        startActivity(myIntent)
+
+//        메인으로 넘어가고 나면, 이 화면 종료
+        finish()
+
     }
 }
